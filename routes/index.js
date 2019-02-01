ac0371d73faec7007e85d65b9d59014c50e01161
Criotech <opesiyanbola8991@gmail.com>
2019-02-01 12:22:29 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -463,7 +463,7 @@ router.get('/dashboard/news', function (req, res, next) {
 
     News.find({}).then((doc) => {
         if (doc) {
-            res.render('backend/news', { upload, doc })
+            res.render('backend/news', { upload, doc, usrInfo })
             console.log(doc)
         } else {
             res.render('backend/news')
